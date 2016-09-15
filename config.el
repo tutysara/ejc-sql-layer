ba4411420a5c945c0884671ef277bcbd11643dff
Saravanan Baskaran <sb098k@att.com>
2016-09-15 11:04:10 -0400

    ready to push to external repo

---
+++ config.el
@@ -0,0 +1,2 @@
+(defvar *my-ejc-sql-connections-file* "~/.my-ejc-sql-connections.el"
+  "The name of file containing the connections")
---
+++ funcs.el
@@ -0,0 +1,2 @@
+(defun my-ejc-sql/load-connection-file (file-name)
+  (load file-name))
